25051bb5b2a62bfd0c75f36c013ee896bd7db2ff
Nicole Thurnau <nthurnau@gmail.com>
2016-06-21 14:37:52 -0700

    set up server

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ var
     express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    logger = require('morgan')
+    path = require('path'),
+    logger = require('morgan'),
+    PORT = process.env.PORT || 3000,
+    apiRoutes = require('./routes/api.js') 
 
-app.use
+//middleware
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(logger())
+app.use(express.static(path.join(__dirname, 'public-prod')))
+
+app.use('/api', apiRoutes) 
+
+//main route 
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '/public-prod/index.html')) 
+})
+
+app.listen(PORT, function(){
+    console.log("everything is running on " + PORT + " everything is good!")
+})
